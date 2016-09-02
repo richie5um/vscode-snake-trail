e286982e436711c4aee71b1381fcb1a3bf6aa81a
Rich Somerfield <rs@richsomerfield.com>
2016-09-02 15:04:14 +0100

    Fixing bug in 'large text' checker

--- extension.ts
+++ extension.ts
@@ -134,7 +134,7 @@ export function activate(context: vscode.ExtensionContext) {
 						triggerUpdateDecorations();
 
 						if (0 <= snakeRange.opacityIndex) {
-							setTimeout(fn, 50 + snakeOptions.fadeMS + (10 * Math.max(snakeRange.rangeLength, 30)));
+							setTimeout(fn, 50 + snakeOptions.fadeMS + (10 * Math.min(snakeRange.rangeLength, 30)));
 						}
 					};
 					setTimeout(fn, snakeOptions.fadeMS);
